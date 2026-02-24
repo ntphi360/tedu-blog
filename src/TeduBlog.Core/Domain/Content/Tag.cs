193d9807e9ba9f77842c8e71bdc614a1944d6260
ntphi360 <ntphi360@gmail.com>
2026-02-24 21:47:01 +0700

    Create entities for post

---
+++ src/TeduBlog.Core/Domain/Content/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AppBlog.Core.Domain.Content
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+    }
+}
